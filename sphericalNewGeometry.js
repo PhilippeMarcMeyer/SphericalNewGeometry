e5fb8bc182b5e3043a2848309e51061c46991430
Philippe MEYER <p.meyer@mappingcontrol.com>
2018-11-05 17:42:14 +0100

    trying to fill faces with different colors

--- sphericalNewGeometry.js
+++ sphericalNewGeometry.js
@@ -9,6 +9,7 @@ var radiusX =150;
 var radiusY=150;
 var radiusZ=150;
   var gId = 123;
+  var camX,camY;
 function setup() {
   
   // we need to remember the renderer that is created so
@@ -18,7 +19,29 @@ function setup() {
   noStroke();
 
 
-  camera(0, -400, 300, 0, 0, 0, 0, -1, 0);
+  	camY = -600;
+	camX = 0;
+	camera(camX, camY, 200, 0, 0, 0, 0,1, 0);
+		var delta;
+	renderer.mouseWheel(function(e){
+	var way = (e.deltaY > 0) ? 1 : -1;
+	if(way == 1){
+		delta = abs(camY) / 40 ;
+	}else{
+		delta = abs(camY) / 10;
+	}
+	if (delta > 1){
+		camY += way * delta ;
+	}else{
+		if(way == -1){
+			camY -= 1 ;
+		}
+
+	}
+
+	camera(camX, camY, 200, 0, 0, 0, 0,-1, 0);
+
+  });
 
 
     var ellipsoid = function() {
@@ -44,6 +67,7 @@ function setup() {
     geometry = new p5.Geometry(definition, definition, ellipsoid);
 
 
+
 }
 
 function draw() {
@@ -53,45 +77,62 @@ function draw() {
   
 //ambientLight(10,10,24);
   // calculate the sun & moon's positions from spherical angles
-  var sunPos = p5.Vector.fromAngles(tt / 5000, PI / 4, 420);
-  var moonPos = p5.Vector.fromAngles(PI + tt / 5000, PI / 4, 1000);
+  var sunPos = p5.Vector.fromAngles(tt / 7000, PI / 4, radiusX*2);
+  var moonPos = p5.Vector.fromAngles(PI + tt / 5000, PI / 2, radiusX*1.5);
   
 
-
+  push();
   fill(62,89,250);
    ambientLight(10);
  // ambientMaterial(10,10,240)
   // add the sun & moon lights
   pointLight(255, 250, 136, sunPos);
-   pointLight(255, 250, 136, sunPos);
-      pointLight(255, 250, 136, sunPos);
+  pointLight(255, 250, 136, sunPos);
+  pointLight(255, 250, 136, sunPos);
 
   pointLight(150, 150, 150, moonPos);
 
   // re-compute the faces 
   geometry.computeFaces()._makeTriangleEdges()._edgesToVertices();
-  
-  geometry.faces.forEach(function(x){
-	  x.color = color(random(250),random(250),random(250));
-  });
-  
+  		 // this.colors.push(color(random(250),random(250),random(250)));
+
+
+  //normalMaterial();
   // update the webgl buffers
   renderer.createBuffers(gId, geometry);
-  
+  rotateX(frameCount * 0.003);
+   rotateY(frameCount * 0.001);
+
   // render the geometry
   renderer.drawBuffersScaled(gId, radiusX, radiusY, radiusZ);
     // add spheres where the lights are going to be
+	  pop();
+
   push();
   fill(245, 231, 46);
   translate(sunPos);
-  sphere(40,24,24);
+  sphere(10);
   pop();
 
   push();
   translate(moonPos);
   fill(255);
-  sphere(40,24,24);
+  sphere(10);
   pop();
-
-  
+  /*
+    push();
+	fill("white");
+  stroke("white");
+var arr = geometry.faces[0]
+var ptA = geometry.vertices[arr[0]].copy().mult(radiusX);
+var ptB = geometry.vertices[arr[1]].copy().mult(radiusX);
+var ptC = geometry.vertices[arr[2]].copy().mult(radiusX);
+  rotateX(frameCount * 0.003);
+   rotateY(frameCount * 0.001);
+line(ptA.x, ptA.y, ptA.z,ptB.x, ptB.y, ptB.z);
+line(ptB.x, ptB.y, ptB.z,ptC.x, ptC.y, ptC.z);
+line(ptC.x, ptC.y, ptC.z,ptA.x, ptA.y, ptA.z);
+noStroke();
+  pop();
+  */
 }
