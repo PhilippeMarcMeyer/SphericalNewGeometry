80155d3a01b822892d84889b872fbd921b6adc1f
Philippe MEYER <p.meyer@mappingcontrol.com>
2018-11-09 13:29:11 +0100

    putting labels over cities

--- sphericalNewGeometry.js
+++ sphericalNewGeometry.js
@@ -3,8 +3,9 @@
 */
 var sunLightPos;
 var renderer;
-var font;
+var fontPtr = null;
 var fontSize = 14;
+var currentRotY =0;
 var geometry;
 var definition = 26;
 var radiusX = 200;
@@ -20,52 +21,141 @@ var orbiter = null;
 var autoRotate = true;
 var lastOrbitCtrlRotations;
 var fr = 8;
+var placesDone = false;
 var places = [
-	{"name":"Paris","lat":48.856614,"lon": 2.3522219}
-]
+	{"name":"Paris","lat":48.856614,"lon": 2.3522219,"offScreen":null},
+	{"name":"London","lat":51.5085300,"lon": -0.1257400,"offScreen":null},
+	{"name":"New York","lat":40.712784,"lon": -74.005941,"offScreen":null}
+
+];
+
+
+function preload() {
+	fontPtr = loadFont("fonts/Roboto-Regular.otf");
+	while(fontPtr==null){
+
+	}
+	preparePlaces();
+	placesDone = true;
+	
+}
+
 
 function setup() {
-  font = loadFont("fonts/Roboto-Regular.otf");
-  renderer = createCanvas(windowWidth, windowHeight-120, WEBGL);
-  lastOrbitCtrlRotations = createVector(0, 0, 0);
-  
-   renderer.mouseOver(function(){
-	   cursor(HAND);
-   });
-  renderer.mousePressed(function(){
-	  	// reproduce orbitControl rotations (trying!)
-	lastOrbitCtrlRotations.x = (mouseX - width / 2) / (width / 2);
-	lastOrbitCtrlRotations.y = (mouseY - height / 2) / (height / 2);
-  });
-  
-    renderer.mouseReleased(function(){
-	cursor(ARROW);
-	lastOrbitCtrlRotations.x = (mouseX - width / 2) / (width / 2);
-	lastOrbitCtrlRotations.y = (mouseY - height / 2) / (height / 2);
-  });
-  
-  renderer.doubleClicked(function() {
-	autoRotate = !autoRotate;
-});
-  
-  frameRate(fr);
-  sunLightPos = createVector(600, 0, -600);
-  noStroke();
-  	camY = -600;
-	camX = 0;
 	
-	camera(camX, camY, 200, 0, 0, 0, 0,1, 0);
-	var delta;
-	renderer.mouseWheel(function(e){
-	var way = (e.deltaY > 0) ? 1 : -1;
-	camY += e.deltaY;
-	if(camY > -200) camY = -200;
-	if(camY < -4000) camY = -4000;
+	renderer = createCanvas(windowWidth, windowHeight-120, WEBGL);
+	lastOrbitCtrlRotations = createVector(0, 0, 0);
 
+	radiusX = radiusY = radiusZ = min(windowHeight,windowWidth) / 4; 
+
+	frameRate(fr);
+	sunLightPos = createVector(600, 0, -600);
+	noStroke();
+
+	setCamera();
+	setListeners();
+
+	if(!usePrimitive) {
+		geometry = new p5.Geometry(definition, definition, makeEllipsoid);
+	}
+
+	loadImage("images/earth2.jpg", function(temp) {
+		earth = temp.get();
+		if(earth){
+			earthLoaded = true;
+		}
+		
+	}, function(event) {
+		console.log(event);
+	});	
+}
+
+function draw() {
+
+	orbiter = orbitControl();
+	background(0);
+	if(earthLoaded){
+		currentRotY+=0.005;
+		var yRot = (autoRotate) ? currentRotY : 0;
+		background(0);
+		push();
+		fill(62,89,250);
+		if(!once){
+		  lastOrbitCtrlRotations.y = radians(180);
+		  lastOrbitCtrlRotations.x = radians(60);
+		}
+		texture(earth);
+		ambientLight(210);
+		directionalLight(255, 250, 136, sunLightPos.x,sunLightPos.y,sunLightPos.z);
+		
+		rotateX(lastOrbitCtrlRotations.x); 
+		rotateY(lastOrbitCtrlRotations.y+yRot); 
+
+		//rotateZ(radians(10)); // real inclination is around 22°
+
+		if(!usePrimitive)  {
+			geometry.computeFaces()._makeTriangleEdges()._edgesToVertices();
+			renderer.createBuffers(gId, geometry);
+			renderer.drawBuffersScaled(gId, radiusX, radiusY,radiusZ);
+		}else{
+			sphere(radiusX,24,24);
+		}
+		
+		pop();
+		
+		if(placesDone){
+			drawPlaces();
+		}
+	}
+}
+
+function setCamera(){
+	camY = -600;
+	camX = 0;
 	camera(camX, camY, 200, 0, 0, 0, 0,1, 0);
+}
+
+function setListeners(){
+	renderer.mouseOver(function(){
+		cursor(HAND);
+	});
+	renderer.mousePressed(function(){
+	// reproduce orbitControl rotations (trying!)
+		currentRotY=0;
+		lastOrbitCtrlRotations.x = (mouseX - width / 2) / (width / 2);
+		lastOrbitCtrlRotations.y = (mouseY - height / 2) / (height / 2);
+	});
+
+	renderer.mouseReleased(function(){
+		cursor(ARROW);
+		currentRotY=0;
+		lastOrbitCtrlRotations.x = (mouseX - width / 2) / (width / 2);
+		lastOrbitCtrlRotations.y = (mouseY - height / 2) / (height / 2);
+	});
+	
+	renderer.doubleClicked(function() {
+		autoRotate = !autoRotate;
+	});
+	
+	renderer.mouseWheel(function(e){
+		var way = (e.deltaY > 0) ? 1 : -1;
+		camY += e.deltaY;
+		if(camY > -200) camY = -200;
+		if(camY < -4000) camY = -4000;
+		camera(camX, camY, 200, 0, 0, 0, 0,1, 0);
   });
 
-var ellipsoid = function() {
+}
+
+window.onresize = function(){
+ radiusX = radiusY = radiusZ = min((windowHeight-120),windowWidth) /3; 
+ resizeCanvas(windowWidth,windowHeight-120);
+ translate(0,0,0);
+ once=false;
+ setCamera()
+};
+
+function makeEllipsoid(){
   for (var i = 0; i <= definition; i++) {
 	var v = i / definition;
 	var phi = Math.PI * v - Math.PI / 2;
@@ -83,92 +173,45 @@ var ellipsoid = function() {
 	  this.uvs.push(u, v);
 	}
   }
-};
-	
-	if(!usePrimitive) geometry = new p5.Geometry(definition, definition, ellipsoid);
-	
-
-    loadImage("images/earth2.jpg", function(temp) {
-		earth = temp.get();
-		if(earth){
-			earthLoaded = true;
-		}
-		
-	}, function(event) {
-		console.log(event);
-	});
-	
-
 }
 
-function draw() {
-	orbiter = orbitControl();
-	background(0);
-	if(earthLoaded){
-		var currentRotY = (autoRotate) ? frameCount * 0.003 : 0;
-		background(0);
-		push();
-		fill(62,89,250);
-		if(!once){
-		  lastOrbitCtrlRotations.y = radians(180);
-		  lastOrbitCtrlRotations.x = radians(60);
-		}
-		texture(earth);
-		ambientLight(210);
-		directionalLight(255, 250, 136, sunLightPos.x,sunLightPos.y,sunLightPos.z);
+function preparePlaces(){
+	//{"name":"Paris","lat":48.856614,"lon": 2.3522219}
+	places.forEach(function(place){
+		var w = fontSize * place.name.length * 0.6;
+	    var h = fontSize + 4;
+		place.offScreen =  createGraphics(w, h);
+		place.offScreen.background(255);
+		place.offScreen.stroke("red");
+		place.offScreen.noFill();
+		place.offScreen.rect(0,0,w-1, h-1);
+		place.offScreen.textSize(fontSize);
+		place.offScreen.fill("red");
+
+		place.offScreen.text(place.name,4,fontSize);
+		place.offScreen.scale(-1,1);
+		place.offScreen.image(place.offScreen, -place.offScreen.width, 0);
+	});
+}
+
+function drawPlaces(){
+	places.forEach(function(place){
+		// fix: in OpenGL, y & z axes are flipped from math notation of spherical coordinates
+		var theta = radians(place.lat) + PI/2 -0.12;
+		var phi = radians(place.lon) - PI/2 -0.01;
+		var r = radiusX;
+
+		var x = r * cos(theta) * cos(phi);
+		var y = -r * sin(theta);
+		var z = -r * cos(theta) * sin(phi);
 		
+	push();
 		rotateX(lastOrbitCtrlRotations.x); 
 		rotateY(lastOrbitCtrlRotations.y+currentRotY); 
-
-		//rotateZ(radians(10)); // real inclination is around 22°
-
-		if(!usePrimitive)  {
-			geometry.computeFaces()._makeTriangleEdges()._edgesToVertices();
-			renderer.createBuffers(gId, geometry);
-			renderer.drawBuffersScaled(gId, radiusX, radiusY,radiusZ);
-		}else{
-			sphere(radiusX,24,24);
-		}
-		places.forEach(function(place){
-			// fix: in OpenGL, y & z axes are flipped from math notation of spherical coordinates
-
-			var theta = radians(place.lat) + PI/2 -0.12;
-			var phi = radians(place.lon) - PI/2 -0.01;
-			var r = radiusX;
-			/*
-			var x = r * sin(theta) * cos(phi);
-			var y = -r * sin(theta) * sin(phi);
-			var z = r * cos(theta);
-			*/
-			var x = r * cos(theta) * cos(phi);
-			var y = -r * sin(theta);
-			var z = -r * cos(theta) * sin(phi);
-			
-			fill("red")
-
-			stroke("red")
-			strokeWeight(6);
-			line(x,y,z,x,y,z);
-			if(font && false){
-			strokeWeight(1);
-			 points = font.textToPoints(place.name, 0, 0, fontSize, {
-				sampleFactor: 5,
-				simplifyThreshold: 0
-			});
-			
-			points.forEach(function(pti,i){
-				line(x + pti.x, y + pti.y,z,x + pti.x, y + pti.y,z);
-			});
-			}
-			//translate(x,y,z);
-			//box(20);
-		});
-
-	pop();
-		
-		
-	}
+		translate(x,y-50,z);
+ 
+		texture(place.offScreen);
+		plane(place.offScreen.width,place.offScreen.height);
+		pop();
+	});
 }
-
-
-
